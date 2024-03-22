ebe1415956aa244b225bcf6b295c1f8a300eb25c
Your Name <you@example.com>
2024-03-22 10:19:31 +0530

    update

--- registerbackend.php
+++ registerbackend.php
@@ -19,6 +19,24 @@ $password = $_POST['password'];
 $address = $_POST['address'];
 
 
+try {
+    // Prepare SQL statement to insert data
+    $stmt = $pdo->prepare("INSERT INTO users (firstname, lastname, email, phoneno, password,address) VALUES (:firstname, :lastname, :email, :phoneno, :password,:address)");
+
+    // Bind parameters
+    $stmt->bindParam(':firstname', $firstname);
+    $stmt->bindParam(':lastname', $lastname);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':phoneno', $phoneno);
+    $stmt->bindParam(':password', $password);
+    $stmt->bindParam(':address', $address);
+    // Execute the statement
+    $stmt->execute();
+
+    echo "User registered successfully!";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
 
 // Close the connection
 $pdo = null;
